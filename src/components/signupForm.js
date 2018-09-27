b0380b7134fa9aad4539288265ecb88f4a73258a
Ozgun Bal <ozgunbal@gmail.com>
2018-09-27 17:23:34 +0300

    changed regex to fix lint errors

--- src/components/signupForm.js
+++ src/components/signupForm.js
@@ -5,44 +5,74 @@ import Button from 'react-toolbox/lib/button/Button';
 import { signUp } from './../api';
 
 class SignupForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            emailError: "",
-            passwordError: "",
-            errorMessage: null,
-        }
-        this.submit = this.submit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    submit() {
-        const { email, password } = this.state;
-        const emailError = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) ? null : "E-mail is invalid";
-        const passwordError = password.length < 8 ? "Password should contain at least 8 character" : null;
-        this.setState({ emailError, passwordError });
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      emailError: '',
+      passwordError: '',
+      errorMessage: null,
+    };
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  submit() {
+    const { email, password } = this.state;
+    const emailRegex = new RegExp(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+    const emailError = emailRegex.test(email) ? null : 'E-mail is invalid';
+    const passwordError =
+      password.length < 8
+        ? 'Password should contain at least 8 character'
+        : null;
+    this.setState({ emailError, passwordError });
 
-        if (!emailError && !passwordError) {
-            signUp(email, password)
-                .then(() => this.props.onSuccess())
-                .catch(error => this.setState({ errorMessage: error.message }))
-        }
+    if (!emailError && !passwordError) {
+      signUp(email, password)
+        .then(() => this.props.onSuccess())
+        .catch(error => this.setState({ errorMessage: error.message }));
     }
-    handleChange = (evt) => {
-        this.setState({ [evt.target.name]: evt.target.value });
-    }
-    render() {
-        const { email, emailError, password, passwordError, errorMessage } = this.state;
-        return (
-            <div className="LoginForm" style={{ textAlign: 'center', justifyContent: 'center' }} onChange={this.handleChange.bind(this)}>
-                <div style={{ display: !!errorMessage, color: 'red' }}><em>{errorMessage}</em></div>
-                <Input type='text' label='E-mail' name='email' value={email} error={emailError} />
-                <Input type='password' label='Password' name='password' value={password} error={passwordError} />
-                <Button onClick={this.submit} label="Sign Up" primary />
-            </div>
-        );
-    }
+  }
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+  render() {
+    const {
+      email,
+      emailError,
+      password,
+      passwordError,
+      errorMessage,
+    } = this.state;
+    return (
+      <div
+        className="LoginForm"
+        style={{ textAlign: 'center', justifyContent: 'center' }}
+        onChange={this.handleChange.bind(this)}
+      >
+        <div style={{ display: !!errorMessage, color: 'red' }}>
+          <em>{errorMessage}</em>
+        </div>
+        <Input
+          type="text"
+          label="E-mail"
+          name="email"
+          value={email}
+          error={emailError}
+        />
+        <Input
+          type="password"
+          label="Password"
+          name="password"
+          value={password}
+          error={passwordError}
+        />
+        <Button onClick={this.submit} label="Sign Up" primary />
+      </div>
+    );
+  }
 }
 
-export default SignupForm
+export default SignupForm;
